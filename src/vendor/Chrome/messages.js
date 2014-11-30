6041607090f1bb96f3c821cefbdfcb7fd9036f59
Anton <anton9121@gmail.com>
2014-11-30 08:17:26 +0500

    1

--- src/vendor/Chrome/messages.js
+++ src/vendor/Chrome/messages.js
@@ -7,28 +7,29 @@
       if (sender.tab) {
         // send to tab
         return function(message) {
-          chrome.tabs.sendMessage(sender.tab.id, message);
+          chromeMsg.sendTo(message, sender.tab.id);
         }
       }
       return function(message) {
         // send to extension
-        chrome.runtime.sendMessage(sender.id, message);
+        chromeMsg.send(message);
       }
     },
     sendTo: function(message, tabId) {
       chrome.tabs.sendMessage(tabId, message);
+    },
+    onMessage: function(message, sender) {
+      var response = chromeMsg.mkResponse(sender);
+      for (var i = 0, cb; cb = chromeMsg.cbList[i]; i++) {
+        cb(message, response);
+      }
     },
     on: function(cb) {
       chromeMsg.cbList.push(cb);
       if (chromeMsg.cbList.length !== 1) {
         return;
       }
-      chrome.runtime.onMessage.addListener(function(message, sender) {
-        var response = chromeMsg.mkResponse(sender);
-        for (var i = 0, cb; cb = chromeMsg.cbList[i]; i++) {
-          cb(message, response);
-        }
-      });
+      chrome.runtime.onMessage.addListener(chromeMsg.onMessage);
     },
     sendToActiveTab: function(message) {
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -42,6 +43,28 @@
       chrome.runtime.sendMessage(message);
     }
   };
+
+  (function() {
+    if (chrome.runtime.getBackgroundPage === undefined) return;
+
+    chrome.runtime.getBackgroundPage(function(bgWin) {
+      var isPopup = true;
+      if (bgWin === window) {
+        isPopup = false;
+      }
+
+      if (isPopup) {
+        chromeMsg.send = mono.sendMessage.send = function(message) {
+          bgWin.mono.chromeDirectOnMessage(message);
+        }
+      } else
+      if (mono.chromeDirectOnMessage === undefined ) {
+        mono.chromeDirectOnMessage = function(message) {
+          chromeMsg.onMessage(message, chrome.runtime.id);
+        };
+      }
+    });
+  })();
 
   mono.onMessage.on = chromeMsg.on;
   mono.sendMessage.send = chromeMsg.send;
