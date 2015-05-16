7a6b481bff013c4e3bdb6fef53044ba651e70b66
Anton <anton9121@gmail.com>
2015-05-16 17:54:24 +0500

    rb strip tags

--- src/components/browserDefine.js
+++ src/components/browserDefine.js
@@ -1,4 +1,5 @@
 (function() {
+  //@strip_firefox_>
   if (typeof window === 'undefined') {
     /**
      * @namespace _require
@@ -6,39 +7,49 @@
     //@include define/firefox.js
     return;
   }
+  //@strip_firefox_<
 
+  //@strip_gm_>
   if (typeof GM_getValue !== 'undefined') {
     //@include define/gm.js
     return;
   }
+  //@strip_gm_<
 
+  //@strip_chrome_>
   if (window.hasOwnProperty('chrome')) {
     //@include define/chrome.js
     //@include define/chromeApp.js
     //@include define/chromeWebApp.js
     return;
   }
+  //@strip_chrome_<
 
+  //@strip_opera_>
   if (window.hasOwnProperty('opera')) {
     //@include define/opera.js
     return;
   }
+  //@strip_opera_<
 
+  //@strip_firefox_>
   if (navigator.userAgent.indexOf('Firefox') !== -1) {
     //@include define/firefox.js
     return;
   }
+  //@strip_firefox_<
 
+  //@strip_safari_>
   if (window.hasOwnProperty('safari')) {
     //@include define/safari.js
     return;
   }
-
   if (navigator.userAgent.indexOf('Safari/') !== -1) {
     // Safari bug!
     mono.isSafari = true;
     return;
   }
+  //@strip_safari_<
 
   console.error('Mono: can\'t define browser!');
 })();
