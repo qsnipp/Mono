9c1df95acb42b8da8e8aa76536da2fc7d90e6644
Anton <anton9121@gmail.com>
2015-05-24 17:37:52 +0500

    fix

--- src/components/define/chromeApp.js
+++ src/components/define/chromeApp.js
@@ -2,7 +2,7 @@ if (!chrome.app.hasOwnProperty('getDetails')) {
   mono.isChromeApp = true;
 } else {
   var details = chrome.app.getDetails();
-  if (details && details.hasOwnProperty('app') !== 'undefined') {
+  if (details && details.hasOwnProperty('app')) {
     mono.isChromeWebApp = true;
   }
   details = undefined;
