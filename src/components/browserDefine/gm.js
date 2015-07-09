7e4250eb883b548a65ee516e6eb7482239c494a9
Anton <anton9121@gmail.com>
2015-07-09 16:00:01 +0500

    1

--- src/components/browserDefine/gm.js
+++ src/components/browserDefine/gm.js
@@ -2,7 +2,8 @@ mono.isGM = true;
 mono.msgType = 'gm';
 if (window.hasOwnProperty('chrome')) {
     mono.isTM = true;
-} else if (navigator.userAgent.indexOf('Maxthon/') !== -1) {
+} else
+if (navigator.userAgent.indexOf('Maxthon/') !== -1) {
     mono.isVM = true;
 } else {
     mono.isGmOnly = true;
--- test/js/mono.js
+++ test/js/mono.js
@@ -60,7 +60,8 @@ var mono = (typeof mono !== 'undefined') ? mono : undefined;
                 mono.msgType = 'gm';
                 if (window.hasOwnProperty('chrome')) {
                     mono.isTM = true;
-                } else if (navigator.userAgent.indexOf('Maxthon/') !== -1) {
+                } else
+                if (navigator.userAgent.indexOf('Maxthon/') !== -1) {
                     mono.isVM = true;
                 } else {
                     mono.isGmOnly = true;
