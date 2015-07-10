c530aacb071a297f3bcbe5a8a48645d25dd129ca
Anton <anton9121@gmail.com>
2015-07-10 13:50:50 +0500

    1

--- test/js/popup.js
+++ test/js/popup.js
@@ -1,7 +1,7 @@
 console.error("Popup page!");
 
-document.body.style.width = '640px';
-document.body.style.height = '480px';
+document.body.style.width = '650px';
+document.body.style.height = '490px';
 document.body.style.margin = 0;
 
 initBase('popup');
